4da459b8d6d14c2f66c0c0344b4969d4efc96d68
Tan Thinh <thinhhtse183224@fpt.edu.vn>
2024-05-23 15:22:51 +0700

    Add Project

---
+++ src/main/java/com/group5/dvs_backend/DvsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.group5.dvs_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group5/dvs_backend/DvsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group5.dvs_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

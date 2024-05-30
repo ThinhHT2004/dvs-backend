553d3a73c1e0ac2d1645c0647a60f0e9dd236b1a
kientdtse183827 <kientdtse183827@fpt.edu.vn>
2024-05-30 21:14:27 +0700

    Create CustomerController.java

---
+++ src/main/java/com/group5/dvs_backend/controller/CustomerController.java
@@ -0,0 +1,36 @@
+package com.group5.dvs_backend.controller;
+
+
+import com.group5.dvs_backend.entity.Customer;
+import com.group5.dvs_backend.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping
+    public List<Customer> showAllCustomer(){
+        return customerService.getAll();
+    }
+    @GetMapping("{id}")
+    public Customer showCustomerById(@PathVariable Long id){
+        return customerService.getCustomerById(id);
+    }
+    public void updateCustomer(@RequestBody Customer updatedCustomer){
+        customerService.updateCustomer(updatedCustomer);
+
+    }
+
+
+
+
+
+
+}

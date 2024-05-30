cd15ff430fac0e82c9fc56db8c16e0792d26cf69
kientdtse183827 <kientdtse183827@fpt.edu.vn>
2024-05-30 20:33:52 +0700

    Create CustomerService.java

---
+++ src/main/java/com/group5/dvs_backend/service/CustomerService.java
@@ -0,0 +1,19 @@
+package com.group5.dvs_backend.service;
+
+import com.group5.dvs_backend.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public List<Customer> getAll(){
+
+    }
+
+
+
+
+}

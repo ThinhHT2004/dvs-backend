7ce086fe24411f9e1b8d260f8102a3a2abdce5f9
kientdtse183827 <kientdtse183827@fpt.edu.vn>
2024-05-30 21:25:08 +0700

    Update CustomerRepository.java

--- src/main/java/com/group5/dvs_backend/repository/CustomerRepository.java
+++ src/main/java/com/group5/dvs_backend/repository/CustomerRepository.java
@@ -1,9 +1,9 @@
 package com.group5.dvs_backend.repository;
 
-import com.group5.dvs_backend.entity.Account;
+import com.group5.dvs_backend.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository extends JpaRepository<Account, Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }

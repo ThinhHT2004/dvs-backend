07903b993fccf83f18c029fe70674b7c30aae7dd
kientdtse183827 <kientdtse183827@fpt.edu.vn>
2024-05-30 20:14:30 +0700

    Update Customer.java

--- src/main/java/com/group5/dvs_backend/entity/Customer.java
+++ src/main/java/com/group5/dvs_backend/entity/Customer.java
@@ -3,6 +3,7 @@ package com.group5.dvs_backend.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Customer {
@@ -16,9 +17,62 @@ public class Customer {
     private String address;
     private Date dob;
 
+    public Customer() {
+    }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getFirst_name() {
+        return first_name;
+    }
 
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
 
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
 }

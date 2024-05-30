9ccd2c28c3aa63e74d611033fb8d7059e24307f8
kientdtse183827 <kientdtse183827@fpt.edu.vn>
2024-05-30 21:14:32 +0700

    Update CustomerService.java

--- src/main/java/com/group5/dvs_backend/service/CustomerService.java
+++ src/main/java/com/group5/dvs_backend/service/CustomerService.java
@@ -1,19 +1,27 @@
 package com.group5.dvs_backend.service;
 
+import com.group5.dvs_backend.entity.Account;
+import com.group5.dvs_backend.entity.Customer;
 import com.group5.dvs_backend.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
 
-    public List<Customer> getAll(){
-
+    public List<Customer> getAll() {
+        return customerRepository.findAll();
     }
 
+    public Customer getCustomerById(Long id) {
+        return customerRepository.findById(id).orElse(null);
+    }
 
-
-
+    public void updateCustomer(Customer updatedCustomer) {
+        customerRepository.save(updatedCustomer);
+    }
 }

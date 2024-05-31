c41f2fb44d3bff109ffba71c481fa7dc33dab821
kientdtse183827 <kientdtse183827@fpt.edu.vn>
2024-05-31 17:07:23 +0700

    Update UserService.java

--- src/main/java/com/group5/dvs_backend/service/UserService.java
+++ src/main/java/com/group5/dvs_backend/service/UserService.java
@@ -1,6 +1,7 @@
 package com.group5.dvs_backend.service;
 
 import com.group5.dvs_backend.entity.Account;
+import com.group5.dvs_backend.entity.Customer;
 import com.group5.dvs_backend.repository.AccountRepository;
 import com.group5.dvs_backend.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,20 +15,40 @@ public class UserService {
 
     private final BCryptPasswordEncoder passwordEncoder = new BCruptPasswordEncoder();
 
-    public Account registerUser(String username,String password , String confirmPassword){
-        if(accountRepository.findByUsername(username).isPresent()){
+    public Account registerUser(String username,String password , String confirmPassword) {
+        if (accountRepository.findByUsername(username).isPresent()) {
             throw new RuntimeException("User already exist!");
         }
 
-        if(!password.equals(confirmPassword)){
+        if (!password.equals(confirmPassword)) {
             throw new RuntimeException("Passwords do not match!");
         }
         Account account = new Account();
         account.setUsername(username);
         account.setPassword(passwordEncoder.encode(password));
+        account.setRole("Customer");
+
+        Account savedAccount = accountRepository.save(account);
+
+        Customer customer = new Customer();
+        customer.setId(savedAccount.getId());
+        customerRepository.save(customer);
+
+        return savedAccount;
+    }
+    public Account loginUser(String username,String password){
+        Account account = accountRepository.findByUsername(username).orElse(null);
+
+        if(!passwordEncoder.matches(password,account.getPassword())){
+            throw new RuntimeException("Wrong password!");
+        }
+        return account;
+    }
+
+
 
     }
 
 
 
-}
+

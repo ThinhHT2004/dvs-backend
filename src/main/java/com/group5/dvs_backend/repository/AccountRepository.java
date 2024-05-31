1fc84f0d8cc8531a8ecd65f1accf92010337a4bc
kientdtse183827 <kientdtse183827@fpt.edu.vn>
2024-05-31 16:33:30 +0700

    Update AccountRepository.java

--- src/main/java/com/group5/dvs_backend/repository/AccountRepository.java
+++ src/main/java/com/group5/dvs_backend/repository/AccountRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.group5.dvs_backend.entity.Account;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByUsername(String username);
 }

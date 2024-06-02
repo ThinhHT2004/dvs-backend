fb187b45d83e3b527e218812f63dee063af2139e
kientdtse183827 <kientdtse183827@fpt.edu.vn>
2024-06-02 16:41:02 +0700

    Update ValuationRequestController.java

--- src/main/java/com/group5/dvs_backend/controller/ValuationRequestController.java
+++ src/main/java/com/group5/dvs_backend/controller/ValuationRequestController.java
@@ -25,7 +25,7 @@ public class ValuationRequestController {
 
     @GetMapping("/waiting")
     public List<ValuationRequest> getWaitingRequests() {
-        return valuationRequestService.getRequestsByStatus("Waiting");
+        return valuationRequestService.getDetailedWaitingRequest();
     }
 
     @PutMapping("/{requestId}/assign/{consultingStaffId}")

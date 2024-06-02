a07a1a9ebdb75837e592853a1b48e4da44a8a783
kientdtse183827 <kientdtse183827@fpt.edu.vn>
2024-06-02 16:42:34 +0700

    Update ValuationRequestService.java

--- src/main/java/com/group5/dvs_backend/service/ValuationRequestService.java
+++ src/main/java/com/group5/dvs_backend/service/ValuationRequestService.java
@@ -3,18 +3,27 @@ package com.group5.dvs_backend.service;
 import java.util.Collections;
 import java.util.List;
 
+import com.group5.dvs_backend.entity.ValuationReport;
 import com.group5.dvs_backend.entity.ValuationRequest;
+import com.group5.dvs_backend.entity.ValuationRequestDetail;
 import com.group5.dvs_backend.exception.ResourceNotFoundException;
+import com.group5.dvs_backend.repository.ValuationReportRepository;
+import com.group5.dvs_backend.repository.ValuationRequestDetailRepository;
 import com.group5.dvs_backend.repository.ValuationRequestRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class ValuationRequestService {
+    @Autowired
     private final ValuationRequestRepository valuationRequestRepository;
+    @Autowired
+    private final ValuationRequestDetailRepository valuationRequestDetailRepository;
+    @Autowired
+    private final ValuationReportRepository valuationReportRepository;
 
-    public ValuationRequestService(ValuationRequestRepository valuationRequestRepository) {
-        this.valuationRequestRepository = valuationRequestRepository;
-    }
 
     public List<ValuationRequest> getAll() {
         return valuationRequestRepository.findAll();
@@ -25,11 +34,22 @@ public class ValuationRequestService {
     }
 
     public void saveValuationRequestInfor(ValuationRequest valuationRequest) {
-        this.valuationRequestRepository.save(valuationRequest);
+        ValuationRequest savedValuationRequest = valuationRequestRepository.save(valuationRequest);
+        for (int i = 0; i < savedValuationRequest.getQuantity(); i++) {
+            ValuationReport valuationReport = valuationReportRepository.save(new ValuationReport());
+            ValuationRequestDetail valuationRequestDetail = valuationRequestDetailRepository
+                    .save(new ValuationRequestDetail(
+                            savedValuationRequest, valuationReport , "WAITING", 0.0, false)
+                    );
+        }
     }
 
     public List<ValuationRequest> getRequestsByStatus(String status) {
         return valuationRequestRepository.findByStatus(status);
+    }
+
+    public List<ValuationRequest> getDetailedWaitingRequest(){
+        return valuationRequestRepository.findWaitingRequestWithDetails();
     }
 
     public void assignConsultingStaff(Long requestId, Long consultingStaffId) {

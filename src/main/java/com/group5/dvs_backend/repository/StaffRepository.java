aa69e1d426cd76e6942fcc0fbe8f22c92b75dd60
kientdtse183827 <kientdtse183827@fpt.edu.vn>
2024-06-02 19:26:39 +0700

    Update StaffRepository.java

--- src/main/java/com/group5/dvs_backend/repository/StaffRepository.java
+++ src/main/java/com/group5/dvs_backend/repository/StaffRepository.java
@@ -3,5 +3,8 @@ package com.group5.dvs_backend.repository;
 import com.group5.dvs_backend.entity.Staff;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StaffRepository extends JpaRepository<Staff, Long> {
+    List<Staff> findByRole(String role);
 }

a7a428a7ce828032dd866ad8a40dea542fc67c9a
kientdtse183827 <kientdtse183827@fpt.edu.vn>
2024-06-02 16:18:24 +0700

    Create Service.java

---
+++ src/main/java/com/group5/dvs_backend/entity/Service.java
@@ -0,0 +1,34 @@
+package com.group5.dvs_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "service")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Service {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "duration")
+    private String duration;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "updated_date")
+    private Date updatedDate;
+}

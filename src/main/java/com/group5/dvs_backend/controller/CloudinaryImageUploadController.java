fbc2503d8e978b09fa8776a7456add57b4482843
Tan Thinh <thinhhtse183224@fpt.edu.vn>
2024-06-12 00:08:25 +0700

    Update CloudinaryImageUploadController.java

--- src/main/java/com/group5/dvs_backend/controller/CloudinaryImageUploadController.java
+++ src/main/java/com/group5/dvs_backend/controller/CloudinaryImageUploadController.java
@@ -1,19 +1,18 @@
 package com.group5.dvs_backend.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.group5.dvs_backend.service.CloudinaryService;
 
 @RestController
-@RequestMapping("/cloudinary")
+@RequestMapping("/api/cloudinary")
+@CrossOrigin
 public class CloudinaryImageUploadController {
 
     private final CloudinaryService cloudinaryService;
@@ -23,8 +22,9 @@ public class CloudinaryImageUploadController {
     }
 
     @PostMapping("/upload")
-    public ResponseEntity<Map> uploadImage(@RequestParam("image") MultipartFile file, Long valuationReportId) {
-        Map data = this.cloudinaryService.uploadToCloudinary(file, valuationReportId);
+    @CrossOrigin
+    public ResponseEntity<List<Map>> uploadImage(@RequestParam MultipartFile file1, @RequestParam MultipartFile file2,@RequestParam Long valuationReportId) {
+        List<Map> data = this.cloudinaryService.uploadToCloudinary(file1, file2,valuationReportId);
         return new ResponseEntity<>(data, HttpStatus.OK);
     }
 }

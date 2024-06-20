e9c1966504ffdc03d8786261505cd9d821bd74f5
kientdtse183827 <kientdtse183827@fpt.edu.vn>
2024-06-20 11:44:47 +0700

    Update ValuationReportRepository.java

--- src/main/java/com/group5/dvs_backend/repository/ValuationReportRepository.java
+++ src/main/java/com/group5/dvs_backend/repository/ValuationReportRepository.java
@@ -10,5 +10,7 @@ import java.util.List;
 
 @Repository
 public interface ValuationReportRepository extends JpaRepository<ValuationReport, Long> {
+    @Query("SELECT va FROM ValuationReport va WHERE va.id = ?1")
+    List<ValuationReport> findDiamondByValuationReportId(Long id);
 
 }

a210ed651b241b54898a17a033295484cef92527
kientdtse183827 <kientdtse183827@fpt.edu.vn>
2024-06-30 10:49:05 +0700

    Update Service.java

--- src/main/java/com/group5/dvs_backend/service/Service.java
+++ src/main/java/com/group5/dvs_backend/service/Service.java
@@ -5,4 +5,8 @@ import java.util.List;
 public interface Service {
     com.group5.dvs_backend.entity.Service getById(Long id);
     List<com.group5.dvs_backend.entity.Service> getAll();
+    com.group5.dvs_backend.entity.Service updateService(Long id, Service updatedService);
+
+
+    com.group5.dvs_backend.entity.Service updateService(Long id, com.group5.dvs_backend.entity.Service updatedService);
 }

019fb11a2af2d989dd61d3949e617208d6ecf6c7
kientdtse183827 <kientdtse183827@fpt.edu.vn>
2024-06-30 10:49:00 +0700

    Update StaffService.java

--- src/main/java/com/group5/dvs_backend/service/StaffService.java
+++ src/main/java/com/group5/dvs_backend/service/StaffService.java
@@ -3,11 +3,12 @@ package com.group5.dvs_backend.service;
 import com.group5.dvs_backend.entity.Staff;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface StaffService {
     List<Staff> getAll();
     Staff getStaffById(Long id);
-    void updateStaff(Staff staff);
+    Staff updateStaff(Staff staff);
     List<Staff> getStaffByRole(String role);
+    Staff updateStaff̣̣̣̣(Staff updatedStaff);
+
 }
